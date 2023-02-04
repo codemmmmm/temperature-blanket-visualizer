ffe94bb9ad3bafab3a9609c31219145de3fe30f4
codemmmmm <77789413+codemmmmm@users.noreply.github.com>
2023-02-04 17:05:04 +0100

    fixed date format

--- csv_to_html.py
+++ csv_to_html.py
@@ -1,15 +1,14 @@
-# weather station 08546
+# using weather station 08546's data for year 2022
 # https://dev.meteostat.net/bulk/daily.html
 import csv
 
-# last day of the year has wrong order but we only display the first 355 anyway
-# in leap year that would apply to last two days?
 columns = 28
 order = 365 - columns
 with open('data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
-        date = row[0][5:]
+        # convert to dd-mm format
+        date = row[0][8:11] + "-" + row[0][5:7]
         min = row[2]
         max = row[3]
         html = f'<div class="day" data-min="{min}" data-max="{max}" style="order: {order}">{date}</div>'
