9d8dcf9c81873bf60719be210e9b2cafd9c0f76e
codemmmmm <77789413+codemmmmm@users.noreply.github.com>
2023-02-04 17:26:52 +0100

    not done

--- docs/index.js
+++ docs/index.js
@@ -1,2 +1,9 @@
-const firstDay = document.querySelector(".day");
-firstDay.addEventListener("click", () => alert("hi"));
+const toggleEl = document.querySelector("#toggle");
+
+toggleEl.addEventListener("click", () => {
+    const blanketEl = document.querySelector(".blanket");
+    // use return value to decide if what button text should be set
+    if (blanketEl.classList.toggle("hidden-text") === true) {
+        // 
+    }        
+});
